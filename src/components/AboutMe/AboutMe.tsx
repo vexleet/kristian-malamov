b67a73a1ee461e996e6141c4846291a472d28e0c
vexleet <abda70226@gmail.com>
2022-09-23 13:10:10 +0200

    clean a bit about me component

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,6 +1,7 @@
 import { Box, Divider, Grid, Typography } from '@mui/material';
 import Image from 'mui-image';
 import { FC } from 'react';
+import WhoAmI from './WhoAmI';
 
 type AboutMeInfo = {
   title: string;
@@ -36,20 +37,7 @@ const AboutMe = () => {
         <Image src="src/assets/images/me.png" height={430} />
       </Grid>
       <Grid xs={7} item>
-        <div>
-          <Typography variant="h6" color="primary">
-            Who am i?
-          </Typography>
-          <Typography variant="h4" color="secondary" fontWeight="bold" mb={2}>
-            I'm Kristian, a Front End Developer
-          </Typography>
-          <Typography variant="body1" color="secondary" lineHeight={1.8}>
-            I am a Frontend Developer based in Denmark, I have been building different noteworthy
-            and highly-scalable applications for a few years by following design trends and using
-            the latest frontend technologies and methodologies. I am motivated and enthusiastic and
-            always eager to learn more.
-          </Typography>
-        </div>
+        <WhoAmI />
         <Box my={4}>
           <Divider />
         </Box>
---
+++ src/components/AboutMe/WhoAmI.tsx
@@ -0,0 +1,22 @@
+import { Typography } from '@mui/material';
+
+const WhoAmI = () => {
+  return (
+    <div>
+      <Typography variant="h6" color="primary">
+        Who am i?
+      </Typography>
+      <Typography variant="h4" color="secondary" fontWeight="bold" mb={2}>
+        I'm Kristian, a Front End Developer
+      </Typography>
+      <Typography variant="body1" color="secondary" lineHeight={1.8}>
+        I am a Frontend Developer based in Denmark, I have been building different noteworthy and
+        highly-scalable applications for a few years by following design trends and using the latest
+        frontend technologies and methodologies. I am motivated and enthusiastic and always eager to
+        learn more.
+      </Typography>
+    </div>
+  );
+};
+
+export default WhoAmI;
